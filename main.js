d7425bda254f27aa228af51af3a247335509938d
Aradhyo Roy <83859573+Aradhyo-Roy@users.noreply.github.com>
2021-09-24 11:21:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="caputered_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version :", ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jI5hRpeQd/model.json',modelLoaded);
+function check(){
+    img=document.getElementById('caputered_image');
+    classifier.classify(img, gotResult);
+    }
+    function gotResult(error, results){
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(results);
+            document.getElementById("result_object_name").innerHTML=results[0].label;
+            document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+        }
+    }
